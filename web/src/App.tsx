6b007071e4128ac0986b0b9deab44bedecd80a95
Fernando Jorge <fernando.jsts@gmail.com>
2022-09-14 20:25:53 -0300

    :package: Add tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./styles/main.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <h1 className="text-3xl font-bold text-white underline">Hello World!</h1>
     </div>
   );
 };
