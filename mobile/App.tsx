20c525b5b658d743c8b6b53b81ceda6d7e4ee3b6
Fernando Jorge <fernando.jsts@gmail.com>
2022-09-16 18:02:05 -0300

    :sparkles: Add home and loading screens

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,11 +2,28 @@ import React from "react";
 
 import { Background } from "./src/components/Background";
 import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+} from "@expo-google-fonts/inter";
+
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
   return (
     <Background>
       <StatusBar style="light" backgroundColor="transparent" translucent />
+      {fontsLoaded ? <Home /> : <Loading />}
     </Background>
   );
 }
