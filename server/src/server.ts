4e319ed92cef67ad61fd63e703ae1da7e27f5295
Fernando Jorge <fernando.jsts@gmail.com>
2022-09-16 19:48:51 -0300

    :construction: Create backend routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,33 @@ const app = express();
 
 // localhost:3333/ads
 
-app.get("/ads", (request, response) => {
-  console.log("Usuário acessou /ads");
+app.get("/games", (request, response) => {
+  return response.json([]);
+});
+
+app.post("/ads", (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get("/games/:id/ads", (request, response) => {
+  const gameId = request.params.id;
+
+  console.log(`Usuário acessou /games/${gameId}/ads`);
 
   return response.json([
     {
       id: 1,
-      name: "primeiro teste",
+      name: "Game",
     },
   ]);
+});
+
+app.get("/ads/:id/discord", (request, response) => {
+  const gameId = request.params.id;
+
+  console.log(`Usuário acessou /ads/${gameId}/discord`);
+
+  return response.json([]);
 });
 
 app.listen(3333);
