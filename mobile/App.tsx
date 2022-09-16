2134fee42b683599e74eb4259ec560a260ed271a
Fernando Jorge <fernando.jsts@gmail.com>
2022-09-16 17:48:26 -0300

    :wheelchair: improve visibility for status bar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 import { Background } from "./src/components/Background";
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <Background>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
     </Background>
   );
 }
