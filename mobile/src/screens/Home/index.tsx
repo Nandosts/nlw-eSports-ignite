52b903ee0789585266598667c7a3c2ea86d3d78e
Fernando Jorge <fernando.jsts@gmail.com>
2022-09-18 20:15:12 -0300

    :sparkles: Create Duo Cards

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -17,8 +17,8 @@ export function Home() {
 
   const navigation = useNavigation();
 
-  function handleOpenGame() {
-    navigation.navigate("game");
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate("game", { id, title, bannerUrl });
   }
 
   useEffect(() => {
@@ -41,7 +41,7 @@ export function Home() {
             data={games}
             keyExtractor={(item) => item.id}
             renderItem={({ item }) => (
-              <GameCard onPress={handleOpenGame} data={item} />
+              <GameCard onPress={() => handleOpenGame(item)} data={item} />
             )}
             showsHorizontalScrollIndicator={false}
             horizontal
